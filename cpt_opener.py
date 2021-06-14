be93fdf9a8cc600f0337e4cc82944fe4264d8eea
AwesomeSam9523 <samakshgupta04@gmail.com>
2021-06-14 22:52:02 +0530

    try 6

--- cpt_opener.py
+++ cpt_opener.py
@@ -5,7 +5,7 @@ from subprocess import call
 def exec_cmd():
     #call('r"C:/Program Files/nodejs/node.exe" "C:/Users/Administrator/Downloads/bot/bot/cpt/index.js"', shell=True)
     #os.system("start cpt/index-win.exe")
-    os.system("C:/Users/Administrator/Downloads/bot/bot/cpt/index-win.exe")
+    os.system("cpt/index-win.exe")
     #os.system("cpt_opener.bat")
     #call("node C:/Users/Administrator/Downloads/bot/bot/cpt/index.js", shell=True)
 threading.Thread(target = exec_cmd).start()
